b6ebc077bb41e2018f902f83fdf27bdf7bfa1dd4
mchouhy <marianochouhy@gmail.com>
2024-06-15 16:35:10 +0200

    install nodemailer & configuration

---
+++ src/services/email.js
@@ -0,0 +1,61 @@
+import nodemailer from "nodemailer";
+
+export class EmailManager {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      port: 587,
+      auth: {
+        user: "marianochouhy@gmail.com",
+        pass: "5307Pjalpaul",
+      },
+    });
+  }
+
+  async checkoutEmail(email, first_name, ticket) {
+    try {
+      const mailOptions = {
+        from: "Email de prueba <marianochouhy@gmail.com>",
+        to: email,
+        subject: "Confirmación de compra",
+        html: `
+                    <h1>Confirmación de compra</h1>
+                    <p>Gracias por tu compra, ${first_name}!</p>
+                    <p>El número de tu orden es: ${ticket} ¡Guardalo y no lo pierdas!</p>
+                `,
+      };
+
+      await this.transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error("Error al enviar el correo electrónico:", error);
+    }
+  }
+
+  async passwordResetEmail(email, first_name, token) {
+    try {
+      const mailOptions = {
+        from: "marianochouhy@gmail.com",
+        to: email,
+        subject: "Restablecimiento de Contraseña",
+        html: `
+                    <h1>Restablecimiento de Contraseña</h1>
+                    <br />
+                    <p>Hola ${first_name},</p>
+                    <p>Has solicitado restablecer tu contraseña. Utiliza el siguiente código para cambiar tu contraseña:</p>
+                    <br />
+                    <p><strong>${token}</strong></p>
+                    <br />
+                    <p>Este código expirará en 1 hora.</p>
+                    <br />
+                    <a href="http://localhost:8080/password">Restablecer Contraseña</a>
+                    <p>Si no solicitaste este restablecimiento, ignora este correo.</p>
+                `,
+      };
+
+      await this.transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error("Error al enviar correo electrónico:", error);
+      throw new Error("Error al enviar correo electrónico");
+    }
+  }
+}
